f4183ed26f1ee2ae83863c8fe89cc95a4fe83449
Maryna Podina <m.rodina1210@gmail.com>
2022-06-28 03:42:15 +0200

    /

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
         // write your code here
 
-        MainMenu a = new MainMenu();
+        MainMenu a = new MainMenu(false,null);
 
         a.setBounds(200,0,900,900);
         a.setVisible(true);
