558f94435e05106dd06e15701391cbf4d053193b
Maryna Podina <m.rodina1210@gmail.com>
2022-06-28 05:10:21 +0200

    hardlvl check change

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException, UnsupportedAudioFileException {
-        // write your code here
-
-
-
         MainMenu a = new MainMenu(false,null);
-
-
         a.setBounds(200,0,900,900);
         a.setVisible(true);
-
-
-
     }
 
 }
